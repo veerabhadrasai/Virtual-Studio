3196fe39553cad2b508b02d10b35b691ed9150af
veerabhadrasai <veeraallamsetti@gmail.com>
2023-12-31 19:31:32 +0530

    upadata in contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,8 +5,11 @@ const Contact = () => {
   return (
     <div>
       <h2 id='aboutus'>Contact Us</h2>
+      <p id='c_mes'>If you have any NOTES which is Not avaibable in this Site please send to me using giving contact details </p>
+      <br/>
       <p>Name : VS </p>
       <p>Email : Comming soon...</p>
+
       <br/>
       
 
